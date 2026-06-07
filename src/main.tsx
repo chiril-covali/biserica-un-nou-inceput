8958c36b6210ba358148d1e3fb15efe34371b0fd
chiril-covali <cc1qaz@gmail.com>
2026-06-07 23:28:54 +0300

    feat: add Vercel Speed Insights and Analytics integrations

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,11 @@
 import { createRoot } from "react-dom/client";
+import { injectSpeedInsights } from "@vercel/speed-insights";
+import { inject } from "@vercel/analytics";
 import App from "./App.tsx";
 import "./index.css";
 import "./i18n";
+
+injectSpeedInsights();
+inject();
 
 createRoot(document.getElementById("root")!).render(<App />);
